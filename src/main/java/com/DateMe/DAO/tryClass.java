43bd54cd4143a3aea7a75b9e777d61f0d5843089
Nishan <nisanstha355@gmail.com>
2024-09-19 21:22:16 -0400

    try class adding

---
+++ src/main/java/com/DateMe/DAO/tryClass.java
@@ -0,0 +1,11 @@
+package com.DateMe.DAO;
+
+public class tryClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hwllo world");
+
+	}
+
+}
